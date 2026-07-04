fb5e9dc1da70f5f8b0468bdadaf848d0e948bebd
jbasak <jkbasak@outlook.com>
2026-07-04 18:36:49 -0500

    Automated update: Sat 07/04/2026 18:36:48.92

---
+++ git_push_allchanges.bat
@@ -0,0 +1,17 @@
+@echo off
+:: Navigate to your project folder
+cd /d "C:\BASAK\Codebase\github_repos\04_utility_scripts"
+
+:: Stage all changed and new files
+git add .
+
+:: Commit changes with a timestamped message
+git commit -m "Automated update: %date% %time%"
+
+:: Push changes to the default remote branch
+git push origin
+
+echo Changes successfully pushed to 
+
+:: Navigate to your script folder
+cd /d "C:\BASAK\Codebase\github_repos\04_utility_scripts"
